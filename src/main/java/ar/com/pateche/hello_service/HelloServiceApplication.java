f2f844ce71f5df96b887dd5cca5e1a04adfb595a
PatEche <echeverriaple@gmail.com>
2026-05-24 18:44:59 -0300

    first commit

---
+++ src/main/java/ar/com/pateche/hello_service/HelloServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.pateche.hello_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/pateche/hello_service/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.pateche.hello_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
